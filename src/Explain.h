c83f22ccf54b967478a1bc050deb3f92d2114ca8
David Zhao <taipan.snake13@gmail.com>
2017-07-28 16:43:28 +1000

    fixed formatting

--- src/Explain.h
+++ src/Explain.h
@@ -745,10 +745,10 @@ public:
                         "----------\n"
                         "setdepth <depth>: Set a limit for printed derivation tree height\n"
                         "explain <relation>(<element1>, <element2>, ...): Prints derivation tree\n"
-                        "subproof <relation>(<label>): Prints derivation tree for a subproof, label is generated if a derivation tree exceeds height limit\n"
+                        "subproof <relation>(<label>): Prints derivation tree for a subproof, label is "
+                        "generated if a derivation tree exceeds height limit\n"
                         "rule <rule number>: Prints a rule\n"
-                        "exit: Exits this interface\n\n"
-                        );
+                        "exit: Exits this interface\n\n");
             } else if (command[0] == "exit") {
                 break;
             }
