5e534185d7258b191e5f1e0e56b71eefc13d774d
David Zhao <taipan.snake13@gmail.com>
2017-07-31 01:35:24 +1000

    updated provenance explain

--- src/ExplainProvenance.h
+++ src/ExplainProvenance.h
@@ -10,9 +10,10 @@ namespace souffle {
 class ExplainProvenance {
 private:
     SouffleProgram& prog;
+    SymbolTable& symTable;
 
 public:
-    ExplainProvenance(SouffleProgram& prog) : prog(prog) {}
+    ExplainProvenance(SouffleProgram& prog, SymbolTable& symTable) : prog(prog), symTable(symTable) {}
 
     virtual void setup();
     virtual TreeNode explain(std::string relName, std::vector<RamDomain> tuple);
