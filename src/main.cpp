f88721ed4cec0951b65d4048e6eb44cfccc2602b
Bernhard Scholz <Bernhard.Scholz@sydney.edu.au>
2017-12-08 22:55:19 +1100

    Fixed mcpp invocation

--- src/main.cpp
+++ src/main.cpp
@@ -244,7 +244,7 @@ int main(int argc, char** argv) {
     }
 
     /* Create the pipe to establish a communication between cpp and souffle */
-    std::string cmd = "mcpp";
+    std::string cmd = ::which("mcpp");
 
     if (!isExecutable(cmd)) {
         ERROR("failed to locate mcpp pre-processor");
