8af5b5370d885c9673d13a02422885fc47100c52
Martin McGrane <mmcgrane@it.usyd.edu.au>
2018-02-09 13:42:07 +1100

    Deploy commits on master to unstable repo.

--- .travis/bintray_json.sh
+++ .travis/bintray_json.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 DATE=`date --iso-8601`
+if [ -z $1 ]
+then
+  echo "No distribution name was given"
+  exit 1
+else
+  DIST=$1
+fi
+if [ -z $2 ]
+then
+  REPO=deb
+  VERSION=`git describe --abbrev=0 --tags`
+else
+  REPO=deb-unstable
+  VERSION="`git describe --abbrev=0 --tags`-`git rev-parse --short HEAD`" 
+fi
 
-cat > bintray-latest.json <<EOF
+cat > bintray.json <<EOF
 {
     "package": {
         "name": "souffle",
-        "repo": "deb",
+        "repo": "$REPO",
         "subject": "souffle-lang",
         "website_url": "http://souffle-lang.org/",
         "issue_tracker_url": "https://github.com/souffle-lang/souffle/issues",
@@ -19,7 +34,7 @@ cat > bintray-latest.json <<EOF
     },
 
     "version": {
-        "name": "Development",
+        "name": "$VERSION",
         "desc": "HEAD from development branch",
         "released": "$DATE",
         "gpgSign": false
@@ -28,7 +43,7 @@ cat > bintray-latest.json <<EOF
     "files":
     [{"includePattern": "deploy/(.*\.deb)", "uploadPattern": "pool/main/s/souffle/\$1",
     "matrixParams": {
-        "deb_distribution": "xenial",
+        "deb_distribution": "$DIST",
         "deb_component": "main",
         "deb_architecture": "amd64",
         "override": 1 }
