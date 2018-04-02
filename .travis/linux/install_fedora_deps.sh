06e18cf403d4ccef8465e23559856c910a1c6a4f
Martin McGrane <mmcgrane@it.usyd.edu.au>
2018-04-02 15:03:23 +1000

    Add packaging for fedora rpm.

---
+++ .travis/linux/install_fedora_deps.sh
@@ -0,0 +1,1 @@
+yum install -y autoconf automake bison clang doxygen flex gcc gcc-c++ git kernel-devel ncurses-devel sqlite-devel libtool make mcpp pkg-config python sqlite sudo zlib-devel
---
+++ .travis/linux/make_rpm_package.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Enable debugging and logging of shell operations
+# that are executed.
+set -e
+set -x
+
+./bootstrap
+./configure --prefix=usr/local
+
+make -j2 install
+
+#libstdc++ > 4.8 is not sufficient for all of C++11 but 2018/4/2 it does compile and all but provenance works.
+#4.8 is all that's available on centos7 without workarounds
+fpm -t rpm -n souffle -v `git describe --tags --always` -d gcc-c++ -d graphviz -d libgomp \
+	-d libstdc++ > 4.8 -d mcpp -d ncurses-libs -d sqlite-libs -d zlib \
+	--license UPL -s dir usr
+
+mkdir deploy
+# compute md5 for package &
+# copy files to deploy directory
+for f in *.rpm
+do
+  pkg=`basename $f .rpm`
+  src="$pkg.rpm"
+  dest="deploy/$pkg.rpm"
+  cp $src $dest
+  md5sum <$src >deploy/$pkg.md5
+done
+
+# show contents of deployment
+ls deploy/*
