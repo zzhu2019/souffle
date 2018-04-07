717a1e94a090d35d2159c85ceb5b75f327d4fabf
Martin McGrane <mmcgrane@it.usyd.edu.au>
2018-04-07 12:50:28 +1000

    Souffle exits with an error code of 1 on failed compile steps.

--- src/main.cpp
+++ src/main.cpp
@@ -490,6 +490,7 @@ int main(int argc, char** argv) {
             }
         } catch (std::exception& e) {
             std::cerr << e.what() << std::endl;
+            std::exit(1);
         }
     }
 
