c2dc72cb22e2f0cf3055175d7c72ebbeaf7d9fd5
Martin McGrane <mmcgrane@it.usyd.edu.au>
2018-04-13 16:28:06 +1000

    Allow profile log entries to have more flexible ordering.

--- src/profilerlib/Iteration.cpp
+++ src/profilerlib/Iteration.cpp
@@ -13,26 +13,21 @@
 void Iteration::addRule(std::vector<std::string> data, std::string rec_id) {
     std::string strTemp = data[4] + data[3] + data[2];
 
+    std::shared_ptr<Rule> rul_rec = nullptr;
+    if (rul_rec_map.find(strTemp) == rul_rec_map.end()) {
+        rul_rec = std::make_shared<Rule>(Rule(data[4], std::stoi(data[2]), rec_id));
+        rul_rec->setRuntime(0);
+        rul_rec_map[strTemp] = rul_rec;
+    } else {
+        rul_rec = rul_rec_map[strTemp];
+    }
+
     if (data[0].at(0) == 't') {
-        if (rul_rec_map.find(strTemp) != rul_rec_map.end()) {
-            std::shared_ptr<Rule> rul_rec = rul_rec_map[strTemp];
-            rul_rec->setRuntime(std::stod(data[5]) + rul_rec->getRuntime());
-        } else {
-            std::shared_ptr<Rule> rul_rec = std::make_shared<Rule>(Rule(data[4], std::stoi(data[2]), rec_id));
-            rul_rec->setRuntime(std::stod(data[5]));
-            rul_rec->setLocator(data[3]);
-            rul_rec_map[strTemp] = rul_rec;
-        }
-
+        rul_rec->setRuntime(std::stod(data[5]) + rul_rec->getRuntime());
+        rul_rec->setLocator(data[3]);
     } else if (data[0].at(0) == 'n') {
-        std::unordered_map<std::string, std::shared_ptr<Rule>>::const_iterator got =
-                rul_rec_map.find(strTemp);
-
-        assert(got != rul_rec_map.end() && "missing t tag");
-        std::shared_ptr<Rule> rul_rec = rul_rec_map[strTemp];
         rul_rec->setNum_tuples(std::stol(data[5]) - prev_num_tuples);
         this->prev_num_tuples = std::stol(data[5]);
-        rul_rec_map[strTemp] = rul_rec;
     }
 }
 
