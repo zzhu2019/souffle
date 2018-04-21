94f4fe11612f5f1f1528f906bc7b7e63229e35de
Abdul Zreika <azre6702@uni.sydney.edu.au>
2018-04-21 20:30:52 +1000

    Fixed formatting.

--- src/AstTransforms.h
+++ src/AstTransforms.h
@@ -360,68 +360,67 @@ public:
  * Transformer that repeatedly executes a sub-transformer while a condition is met
  */
 class WhileTransformer : public MetaTransformer {
-    private:
-        std::function<bool()> condition;
-        std::unique_ptr<AstTransformer> transformer;
-        bool transform(AstTranslationUnit& translationUnit) override;
+private:
+    std::function<bool()> condition;
+    std::unique_ptr<AstTransformer> transformer;
+    bool transform(AstTranslationUnit& translationUnit) override;
 
-    public:
-        WhileTransformer(std::function<bool()> cond, std::unique_ptr<AstTransformer> transformer)
+public:
+    WhileTransformer(std::function<bool()> cond, std::unique_ptr<AstTransformer> transformer)
             : condition(std::move(cond)), transformer(std::move(transformer)) {}
 
-        WhileTransformer(bool cond, std::unique_ptr<AstTransformer> transformer)
+    WhileTransformer(bool cond, std::unique_ptr<AstTransformer> transformer)
             : condition([=]() { return cond; }), transformer(std::move(transformer)) {}
 
-        void setDebugReport() override {
-            if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
-                mt->setDebugReport();
-            } else {
-                transformer = std::make_unique<DebugReporter>(std::move(transformer));
-            }
+    void setDebugReport() override {
+        if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
+            mt->setDebugReport();
+        } else {
+            transformer = std::make_unique<DebugReporter>(std::move(transformer));
         }
+    }
 
-        void setVerbosity(bool verbose) override {
-            this->verbose = verbose;
-            if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
-                mt->setVerbosity(verbose);
-            }
+    void setVerbosity(bool verbose) override {
+        this->verbose = verbose;
+        if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
+            mt->setVerbosity(verbose);
         }
+    }
 
-        std::string getName() const override {
-            return "WhileTransformer";
-        }
+    std::string getName() const override {
+        return "WhileTransformer";
+    }
 };
 
 /**
  * Transformer that repeatedly executes a sub-transformer until no changes are made
  */
 class FixpointTransformer : public MetaTransformer {
-    private:
-        std::unique_ptr<AstTransformer> transformer;
-        bool transform(AstTranslationUnit& translationUnit) override;
+private:
+    std::unique_ptr<AstTransformer> transformer;
+    bool transform(AstTranslationUnit& translationUnit) override;
 
-    public:
-        FixpointTransformer(std::unique_ptr<AstTransformer> transformer)
-            : transformer(std::move(transformer)) {}
+public:
+    FixpointTransformer(std::unique_ptr<AstTransformer> transformer) : transformer(std::move(transformer)) {}
 
-        void setDebugReport() override {
-            if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
-                mt->setDebugReport();
-            } else {
-                transformer = std::make_unique<DebugReporter>(std::move(transformer));
-            }
+    void setDebugReport() override {
+        if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
+            mt->setDebugReport();
+        } else {
+            transformer = std::make_unique<DebugReporter>(std::move(transformer));
         }
+    }
 
-        void setVerbosity(bool verbose) override {
-            this->verbose = verbose;
-            if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
-                mt->setVerbosity(verbose);
-            }
+    void setVerbosity(bool verbose) override {
+        this->verbose = verbose;
+        if (auto* mt = dynamic_cast<MetaTransformer*>(transformer.get())) {
+            mt->setVerbosity(verbose);
         }
+    }
 
-        std::string getName() const override {
-            return "FixpointTransformer";
-        }
+    std::string getName() const override {
+        return "FixpointTransformer";
+    }
 };
 
 }  // end of namespace souffle
