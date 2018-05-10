a7655a44ea10d57657f4e2b044d15eef90b1e3fc
Martin McGrane <mmcgrane@it.usyd.edu.au>
2018-05-10 15:01:54 +1000

    Add duration to test output.

--- src/ProfileDatabase.cpp
+++ src/ProfileDatabase.cpp
@@ -1,4 +1,5 @@
 #include "ProfileDatabase.h"
+#include <chrono>
 
 int main() {
     souffle::ProfileDatabase x;
@@ -6,6 +7,12 @@ int main() {
     x.addSizeEntry({"a", "b", "bkey"}, 2);
     x.addSizeEntry({"a", "c", "bkey"}, 3);
     x.addTextEntry({"a", "x", "akey"}, "blabla");
+    x.addDurationEntry({"a", "x", "akey"},
+            std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::high_resolution_clock().now().time_since_epoch()),
+            std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::high_resolution_clock().now().time_since_epoch()));
+    std::cout << "print(std::cout" << std::endl;
     x.print(std::cout);
 
     std::cout << "\n\nSum of bkey:" << x.computeSum({"a", "bkey"}) << std::endl;
